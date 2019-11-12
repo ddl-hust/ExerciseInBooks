b261222f12a6402f6ef0b4cc3fec418e9b4621f3
ddl-hust <ddl196526@163.com>
2019-11-11 20:28:49 -0500

    update ch8

--- Cpp_Primer/CH8/8.4_5/main.cpp
+++ Cpp_Primer/CH8/8.4_5/main.cpp
@@ -5,8 +5,8 @@ using namespace std;
 //读取文件内容将其存到vector
 int main() {
     fstream fs;
-    fs.open("../text"); //注意文件路径
-    if(!fs) cout<<"open file fail"<<endl;
+    fs.open("../text"); //注意读取文件路径
+    if(!fs) cout<<"open file fail"<<endl;//打开文件之后，判断状态是个好习惯
     string s;
     vector<string> vec;
 //    while(getline(fs,s)) //从fs读取一行到s
